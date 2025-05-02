fadb5795f1929293b053d8e7cc50514508a5484d
Abhishekkv <abhishekkvpnld@gmail.com>
2025-05-02 18:57:18 +0530

    completed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { PokemonProvider } from './context/pokemonContext'
+import {PokemonProvider } from "./context/PokemonContext.jsx"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
